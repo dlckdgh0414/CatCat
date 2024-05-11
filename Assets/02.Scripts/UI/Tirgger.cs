652c6a905f43d6bef7f716d227241156d1e31fed
dlckdgh0414 <ci5976547@gmail.com>
2024-05-11 23:05:44 +0900

    Add: TimeIine

--- Assets/02.Scripts/UI/Tirgger.cs
+++ Assets/02.Scripts/UI/Tirgger.cs
@@ -5,11 +5,18 @@ using UnityEngine;
 public class Tirgger : MonoBehaviour
 {
     [SerializeField] GameObject UI;
+    BoxCollider2D box;
+
+    private void Awake()
+    {
+        box = GetComponent<BoxCollider2D>();
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.name == "Player")
         {
             UI.SetActive(true);
+            box.isTrigger = true;
             Time.timeScale = 0; 
         }
     }
