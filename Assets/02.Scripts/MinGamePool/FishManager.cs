92efe86914430385c09632270237c6fc09abd5db
dlckdgh0414 <ci5976547@gmail.com>
2024-05-17 00:41:16 +0900

    Add:GoldFish

--- Assets/02.Scripts/MinGamePool/FishManager.cs
+++ Assets/02.Scripts/MinGamePool/FishManager.cs
@@ -6,6 +6,8 @@ public class FishManager : MonoBehaviour
 {
     [SerializeField] private float currentTime;
     [SerializeField] private float creatTime = 2f;
+    [SerializeField] private float GetFishSpawnRange;
+    [SerializeField] GameObject GoldFish;
     public GameObject[] FishPrefab;
 
     private void Update()
@@ -16,6 +18,11 @@ public class FishManager : MonoBehaviour
             int FishIdex = Random.Range(0, FishPrefab.Length);
             Instantiate(FishPrefab[FishIdex], transform.position, Quaternion.identity);
             currentTime = 0;
+            GetFishSpawnRange = Random.Range(0, 10);
+            if(GetFishSpawnRange<=2)
+            {
+                Instantiate(GoldFish, transform.position, Quaternion.identity);
+            }
         }
     }
     private void FixedUpdate()
