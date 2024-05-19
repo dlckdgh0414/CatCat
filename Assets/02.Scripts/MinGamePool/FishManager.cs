2b2e77df7b2f60b11d5c4e553574fc168c2e3c03
dlckdgh0414 <ci5976547@gmail.com>
2024-05-20 00:46:05 +0900

    Fix:ObjectPooling

--- Assets/02.Scripts/MinGamePool/FishManager.cs
+++ Assets/02.Scripts/MinGamePool/FishManager.cs
@@ -31,7 +31,45 @@ public class FishManager : MonoBehaviour
     {
         for(int i =0; i<3; i++)
         {
-
+            GameObject FishObj = Instantiate(FishPrefab[0]);
+            FishPool.Push(FishObj);
+            FishObj.SetActive(false);
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            GameObject FishObj2 = Instantiate(FishPrefab[1]);
+            FishPool.Push(FishObj2);
+            FishObj2.SetActive(false);
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            GameObject FishObj3 = Instantiate(FishPrefab[2]);
+            FishPool.Push(FishObj3);
+            FishObj3.SetActive(false);
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            GameObject FishObj3 = Instantiate(FishPrefab[3]);
+            FishPool.Push(FishObj3);
+            FishObj3.SetActive(false);
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            GameObject FishObj4 = Instantiate(FishPrefab[4]);
+            FishPool.Push(FishObj4);
+            FishObj4.SetActive(false);
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            GameObject FishObj5 = Instantiate(FishPrefab[5]);
+            FishPool.Push(FishObj5);
+            FishObj5.SetActive(false);
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            GameObject FishObj6 = Instantiate(FishPrefab[6]);
+            FishPool.Push(FishObj6);
+            FishObj6.SetActive(false);
         }
     }
     private void FixedUpdate()
