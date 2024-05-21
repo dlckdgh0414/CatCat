60ed4096a52aa3b87227493dfbd0c5d30984f09a
dlckdgh0414 <ci5976547@gmail.com>
2024-05-22 01:17:08 +0900

    Add:Trigger

--- Assets/02.Scripts/UI/SceneInit.cs
+++ Assets/02.Scripts/UI/SceneInit.cs
@@ -8,6 +8,11 @@ public class SceneInit : MonoBehaviour
     [SerializeField]
     private CinemachineVirtualCamera vcam;
 
+    private void Awake()
+    {
+        vcam = GetComponent<CinemachineVirtualCamera>();
+    }
+
     private void Start()
     {
         vcam.Follow = GameObject.FindWithTag("Player").transform;
