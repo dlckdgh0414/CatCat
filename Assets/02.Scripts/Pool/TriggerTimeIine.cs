d11999dbd6b82ec97db321f6ba474355b7fb51a9
dlckdgh0414 <ci5976547@gmail.com>
2024-05-27 16:14:37 +0900

    Add:LoadScene is Pool

--- Assets/02.Scripts/Pool/TriggerTimeIine.cs
+++ Assets/02.Scripts/Pool/TriggerTimeIine.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TriggerTimeIine : MonoBehaviour
 {
@@ -8,7 +9,7 @@ public class TriggerTimeIine : MonoBehaviour
     {
         if(collision.CompareTag("Player"))
         {
-
+            UnityEngine.SceneManagement.SceneManager.LoadScene(4);
         }
     }
 }
