8f908c1449292e79ffbb6b74a530c48a0fc2427e
dlckdgh0414 <ci5976547@gmail.com>
2024-06-03 00:42:30 +0900

    Fix : 미니게임설명신

--- Assets/02.Scripts/MIGame/Nextpage.cs
+++ Assets/02.Scripts/MIGame/Nextpage.cs
@@ -7,13 +7,21 @@ public class NextPage : MonoBehaviour
 {
     [SerializeField] private GameObject[] _next;
     [SerializeField] private int _count;
-
+    private int _clik = 1;
     public void NextButtonClik()
     {
-        if(_count<_next.Length)
+        if (_count<=_next.Length)
         {
-            _next[_count].transform.DOMove(new Vector3(-25, 0, 0), 2f);
-            _count += 1;    
+            StartCoroutine(Delay());
         }
+    }
+    
+    private IEnumerator Delay()
+    {
+        _next[_count].transform.DOMove(new Vector3(-25, 0, 0), 2f);
+        _clik += 1;
+        yield return new WaitForSeconds(1f);
+        _count += _clik;
+ 
     }
 }
