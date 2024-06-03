cb27897c094f016cb1e60cbbbd074e450fef6b6a
dlckdgh0414 <ci5976547@gmail.com>
2024-06-04 00:46:21 +0900

    Fex : 미니게임 설명씬

--- Assets/02.Scripts/MIGame/Nextpage.cs
+++ Assets/02.Scripts/MIGame/Nextpage.cs
@@ -7,7 +7,7 @@ public class NextPage : MonoBehaviour
 {
     [SerializeField] private GameObject[] _next;
     [SerializeField] private int _count;
-    private int _clik = 1;
+
     public void NextButtonClik()
     {
         if (_count<=_next.Length)
@@ -19,9 +19,7 @@ public class NextPage : MonoBehaviour
     private IEnumerator Delay()
     {
         _next[_count].transform.DOMove(new Vector3(-25, 0, 0), 2f);
-        _clik += 1;
-        yield return new WaitForSeconds(1f);
-        _count += _clik;
- 
+        _count += 1;
+        yield return new WaitForSeconds(2f);
     }
 }
